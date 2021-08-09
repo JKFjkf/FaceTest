b3ee03dc6f25f5d732175e8f68c54b76707d7101
jasor <1920578919@qq.com>
2021-08-09 18:09:42 +0800

    更新提交Test_5

---
+++ Test_5/test_1.py
@@ -0,0 +1,11 @@
+str1 = "k:1|k1:2|k2:3|k3:4"
+def str2dict(str1):
+    dict1 = {}
+    for iterms in str1.split('|'):
+        key,value = iterms.split(':')
+        dict1[key] = value
+    return dict1
+#字典推导式
+d = {k:int(v) for t in str1.split("|") for k, v in (t.split(":"), )}
+
+print(str2dict(str1))
